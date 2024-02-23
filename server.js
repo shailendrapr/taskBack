a88f248fa475dcd96b9b7a12ea779364df2579a6
Shailendra <shailendraordeepak@gmail.com>
2024-02-23 22:16:53 +0530

    third commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 // Middleware
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.header('Access-Control-Allow-Origin', 'https://taskback-hx4h.onrender.com');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
